Traqueur-dev/PaperSpigot

map cursor, painting, banner pattern and scoreboard display slot

paper-api-generator/src/main/java/io/papermc/generator/rewriter/types/EnumRegistryRewriter.java
@@ -18,52 +18,45 @@ import java.util.function.Supplier;
 
 import static io.papermc.generator.utils.Formatting.quoted;
 
-public class EnumRegistryRewriter<T, A extends Enum<A>> extends SearchReplaceRewriter {
+public class EnumRegistryRewriter<T, A extends Enum<A>> extends EnumRewriter<Holder.Reference<T>, A> {
 
     private final net.minecraft.core.Registry<T> registry;
     private final Supplier<List<ResourceKey<T>>> experimentalKeys;
     private final boolean isFilteredRegistry;
-    private final boolean keyedParam;
+    private final boolean hasParams;
 
-    public EnumRegistryRewriter(final Class<A> rewriteClass, final ResourceKey<? extends Registry<T>> registryKey, final String pattern, final boolean keyedParam) {
+    public EnumRegistryRewriter(final Class<A> rewriteClass, final ResourceKey<? extends Registry<T>> registryKey, final String pattern, final boolean hasParams) {
         super(rewriteClass, pattern, false);
         this.registry = Main.REGISTRY_ACCESS.registryOrThrow(registryKey);
         this.experimentalKeys = Suppliers.memoize(() -> RegistryUtils.collectExperimentalKeys(this.registry));
         this.isFilteredRegistry = FeatureElement.FILTERED_REGISTRIES.contains(registryKey);
-        this.keyedParam = keyedParam;
+        this.hasParams = hasParams;
     }
 
     @Override
-    protected void insert(final SearchMetadata metadata, final StringBuilder builder) {
-        boolean reachEnd = metadata.replacedContent().stripTrailing().endsWith(";");
-
-        List<Holder.Reference<T>> references = this.registry.holders().sorted(Formatting.alphabeticOrder(reference -> reference.key().location().getPath())).toList();
-        for (int i = 0, size = references.size(); i < size; i++) {
-            Holder.Reference<T> reference = references.get(i);
-            ResourceKey<T> resourceKey = reference.key();
-            String pathKey = resourceKey.location().getPath();
-
-            String fieldName = this.rewriteFieldName(reference);
-            String experimentalValue = this.getExperimentalValue(reference);
-            if (experimentalValue != null) {
-                Annotations.experimentalAnnotations(builder, metadata, experimentalValue);
-            }
-
-            builder.append(metadata.indent()).append(fieldName);
-            if (this.keyedParam) {
-                builder.append("(%s)".formatted(quoted(pathKey)));
-            }
-            if (reachEnd && i == size - 1) {
-                builder.append(';');
-            } else {
-                builder.append(',');
-            }
-            builder.append('\n');
-        }
+    protected Iterable<Holder.Reference<T>> getValues() {
+        return this.registry.holders().sorted(Formatting.alphabeticOrder(reference -> reference.key().location().getPath())).toList();
     }
 
-    protected String rewriteFieldName(Holder.Reference<T> reference) {
+    @Override
+    protected String rewriteEnumName(Holder.Reference<T> reference) {
         return Formatting.formatPathAsField(reference.key().location().getPath());
+    }
+
+    @Override
+    protected String rewriteEnumValue(Holder.Reference<T> reference) {
+        if (this.hasParams) {
+            return quoted(reference.key().location().getPath());
+        }
+        return null;
+    }
+
+    @Override
+    protected void rewriteAnnotation(Holder.Reference<T> reference, StringBuilder builder, SearchMetadata metadata) {
+        String experimentalValue = this.getExperimentalValue(reference);
+        if (experimentalValue != null) {
+            Annotations.experimentalAnnotations(builder, metadata, experimentalValue);
+        }
     }
 
     @Nullable

tathaha/Paper

paper-api-generator/src/main/java/io/papermc/generator/rewriter/types/EnumRegistryRewriter.java
@@ -3,7 +3,6 @@ package io.papermc.generator.rewriter.types;
 import com.google.common.base.Suppliers;
 import io.papermc.generator.Main;
 import io.papermc.generator.rewriter.SearchMetadata;
-import io.papermc.generator.rewriter.SearchReplaceRewriter;
 import io.papermc.generator.rewriter.utils.Annotations;
 import io.papermc.generator.utils.Formatting;
 import io.papermc.generator.utils.RegistryUtils;
@@ -13,58 +12,50 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.flag.FeatureElement;
 import net.minecraft.world.flag.FeatureFlags;
 import org.checkerframework.checker.nullness.qual.Nullable;
-import java.util.List;
 import java.util.Set;
 import java.util.function.Supplier;
 
 import static io.papermc.generator.utils.Formatting.quoted;
 
-public class EnumRegistryRewriter<T, A extends Enum<A>> extends SearchReplaceRewriter {
+public class EnumRegistryRewriter<T, A extends Enum<A>> extends EnumRewriter<Holder.Reference<T>, A> {
 
     private final net.minecraft.core.Registry<T> registry;
     private final Supplier<Set<ResourceKey<T>>> experimentalKeys;
     private final boolean isFilteredRegistry;
-    private final boolean keyedParam;
+    private final boolean hasParams;
 
-    public EnumRegistryRewriter(final Class<A> rewriteClass, final ResourceKey<? extends Registry<T>> registryKey, final String pattern, final boolean keyedParam) {
+    public EnumRegistryRewriter(final Class<A> rewriteClass, final ResourceKey<? extends Registry<T>> registryKey, final String pattern, final boolean hasParams) {
         super(rewriteClass, pattern, false);
         this.registry = Main.REGISTRY_ACCESS.registryOrThrow(registryKey);
         this.experimentalKeys = Suppliers.memoize(() -> RegistryUtils.collectExperimentalDataDrivenKeys(this.registry));
         this.isFilteredRegistry = FeatureElement.FILTERED_REGISTRIES.contains(registryKey);
-        this.keyedParam = keyedParam;
+        this.hasParams = hasParams;
     }
 
     @Override
-    protected void insert(final SearchMetadata metadata, final StringBuilder builder) {
-        boolean reachEnd = metadata.replacedContent().stripTrailing().endsWith(";");
-
-        List<Holder.Reference<T>> references = this.registry.holders().sorted(Formatting.alphabeticKeyOrder(reference -> reference.key().location().getPath())).toList();
-        for (int i = 0, size = references.size(); i < size; i++) {
-            Holder.Reference<T> reference = references.get(i);
-            ResourceKey<T> resourceKey = reference.key();
-            String pathKey = resourceKey.location().getPath();
-
-            String fieldName = this.rewriteFieldName(reference);
-            String experimentalValue = this.getExperimentalValue(reference);
-            if (experimentalValue != null) {
-                Annotations.experimentalAnnotations(builder, metadata, experimentalValue);
-            }
-
-            builder.append(metadata.indent()).append(fieldName);
-            if (this.keyedParam) {
-                builder.append("(%s)".formatted(quoted(pathKey)));
-            }
-            if (reachEnd && i == size - 1) {
-                builder.append(';');
-            } else {
-                builder.append(',');
-            }
-            builder.append('\n');
-        }
+    protected Iterable<Holder.Reference<T>> getValues() {
+        return this.registry.holders().sorted(Formatting.alphabeticKeyOrder(reference -> reference.key().location().getPath())).toList();
     }
 
-    protected String rewriteFieldName(Holder.Reference<T> reference) {
+    @Override
+    protected String rewriteEnumName(Holder.Reference<T> reference) {
         return Formatting.formatKeyAsField(reference.key().location().getPath());
+    }
+
+    @Override
+    protected String rewriteEnumValue(Holder.Reference<T> reference) {
+        if (this.hasParams) {
+            return quoted(reference.key().location().getPath());
+        }
+        return null;
+    }
+
+    @Override
+    protected void rewriteAnnotation(Holder.Reference<T> reference, StringBuilder builder, SearchMetadata metadata) {
+        String experimentalValue = this.getExperimentalValue(reference);
+        if (experimentalValue != null) {
+            Annotations.experimentalAnnotations(builder, metadata, experimentalValue);
+        }
     }
 
     @Nullable