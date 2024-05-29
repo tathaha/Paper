Traqueur-dev/PaperSpigot

CraftBlockData mapping

paper-api-generator/src/main/java/io/papermc/generator/rewriter/imports/ImportCollector.java
@@ -1,0 +1,21 @@
+package io.papermc.generator.rewriter.imports;
+
+public interface ImportCollector {
+
+    ImportCollector NO_OP = new ImportCollector() {
+        @Override
+        public String getStaticAlias(final String fqn) {
+            return fqn;
+        }
+
+        @Override
+        public String getTypeName(final Class<?> clazz) {
+            return clazz.getCanonicalName();
+        }
+    };
+
+    String getStaticAlias(String fqn);
+
+    String getTypeName(Class<?> clazz);
+
+}

tathaha/Paper

paper-api-generator/src/main/java/io/papermc/generator/rewriter/context/ImportCollector.java
@@ -1,0 +1,21 @@
+package io.papermc.generator.rewriter.context;
+
+public interface ImportCollector {
+
+    ImportCollector NO_OP = new ImportCollector() {
+        @Override
+        public String getStaticAlias(final String fqn) {
+            return fqn;
+        }
+
+        @Override
+        public String getTypeName(final Class<?> clazz) {
+            return clazz.getCanonicalName();
+        }
+    };
+
+    String getStaticAlias(String fqn);
+
+    String getTypeName(Class<?> clazz);
+
+}